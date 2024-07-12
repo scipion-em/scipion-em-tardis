ac23c638f628a0115233f4b9dd683a39d69c116e
pconesa <pconesa@cnb.csic.es>
2024-07-12 10:36:47 +0200

    first commit

---
+++ tardis/__init__.py
@@ -0,0 +1,133 @@
+# **************************************************************************
+# *
+# * Authors:     J.L. Vilas (jlvilas@cnb.csic.es)
+# *
+# * Centro Nacional de Biotecnologia CNB-CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+import os
+import pwem
+from pyworkflow.utils import Environ
+from .constants import *
+
+
+__version__ = '1.0.0'
+_logo = "icon.png"
+_references = ['Kiewiszz2023','Kiewisz2022']
+
+
+class Plugin(pwem.Plugin):
+    _homeVar = TARDIS_HOME
+    _pathVars = [TARDIS_HOME]
+    _url = 'https://smlc-nysbc.github.io/TARDIS'
+
+    @classmethod
+    def _defineVariables(cls):
+        cls._defineEmVar(TARDIS_HOME, TARDIS_FOLDER + '-' + TARDIS_VERSION)
+        cls._defineVar(TARDIS_ENV_ACTIVATION, DEFAULT_ACTIVATION_CMD)
+
+    @classmethod
+    def getEnviron(cls, gpuId='0'):
+        """ Setup the environment variables needed to launch Tardis. """
+        environ = Environ(os.environ)
+        if 'PYTHONPATH' in environ:
+            # this is required for python virtual env to work
+            del environ['PYTHONPATH']
+
+        environ.update({'CUDA_VISIBLE_DEVICES': gpuId})
+        return environ
+
+    @classmethod
+    def getTardisEnvActivation(cls):
+        return cls.getVar(TARDIS_ENV_ACTIVATION)
+
+    @classmethod
+    def runTardis(cls, protocol, program, args, cwd=None, gpuId='0'):
+        program = cls.getTardisProgram(program)
+        fullProgram = '%s %s && %s' % (cls.getCondaActivationCmd(), cls.getTardisEnvActivation(), program)
+        protocol.runJob(fullProgram, args, env=cls.getEnviron(gpuId=gpuId), cwd=cwd)
+
+    @classmethod
+    def getTardisProgram(cls, program):
+        return os.path.join(cls.getHome(), 'bin', '%s' % program)
+
+    @classmethod
+    def getDependencies(cls):
+        neededProgs = []
+        condaActivationCmd = cls.getCondaActivationCmd()
+        if not condaActivationCmd:
+            neededProgs.append('conda')
+
+        return neededProgs
+
+    @classmethod
+    def defineBinaries(cls, env):
+        cls.addTardisPackage(env, TARDIS_VERSION)
+
+    @classmethod
+    def addTardisPackage(cls, env, version):
+
+        TARDIS_INSTALLED = 'tardis_%s_installed' % version
+        env_name = getTardisEnvName(version)
+        # try to get CONDA activation command
+        installationCmd = cls.getCondaActivationCmd()
+
+        # Create the environment
+        installationCmd += 'conda create -y -n %s python=3.11 && ' \
+                           % env_name
+
+        # Activate new the environment
+        installationCmd += 'conda activate %s && ' % env_name
+
+        # Install downloaded code
+        installationCmd += 'git clone https://github.com/SMLC-NYSBC/TARDIS.git && '
+        installationCmd += 'cd TARDIS && '
+        installationCmd += 'pip install . && '
+
+        # Flag installation finished
+        installationCmd += 'touch %s' % TARDIS_INSTALLED
+
+        tardis_commands = [(installationCmd, TARDIS_INSTALLED)]
+
+        env.addPackage(TARDIS_FOLDER,
+                       version=TARDIS_VERSION,
+                       tar='void.tgz',
+                       commands=tardis_commands,
+                       neededProgs=cls.getDependencies(),
+                       default=True)
+
+    @classmethod
+    def getTardisCmd(cls, program):
+        """ Composes a Tardis command for a given program. """
+
+        # Program to run
+        program = cls.getTardisProgram(program)
+
+        fullProgram = ' %s %s && %s' % (cls.getCondaActivationCmd(), cls.getTardisEnvActivation(), program)
+
+        # Command to run
+        cmd = fullProgram
+
+        return cmd
+
+
+
+
---
+++ tardis/protocols/protocol_membrane_prediction_base.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     you (you@yourinstitution.email)
+# *
+# * your institution
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'you@yourinstitution.email'
+# *
+# **************************************************************************
+from pyworkflow.protocol import Protocol, params, Integer
+from pyworkflow.utils import Message
+
+from .. import Plugin
+
+
+class MembranePredictionBase(Protocol):
+    """
+    This protocol will print hello world in the console
+    IMPORTANT: Classes names should be unique, better prefix them
+    """
+    _label = 'membrane prediction 3D'
+
+    # -------------------------- DEFINE param functions ----------------------
+    def _defineParams(self, form):
+        """ Define the input parameters that will be used.
+        Params:
+            form: this is the form to be populated with sections and params.
+        """
+        # You need a params to belong to a section:
+        form.addSection(label='Input')
+        form.addParam('inputSetOfTomograms',
+                      params.PointerParam,
+                      pointerClass='SetOfTomograms',
+                      important=True,
+                      label='Tomograms',
+                      help='Set of tomogram to segment membranes.')
+
+    # --------------------------- STEPS functions ------------------------------
+    def _insertAllSteps(self):
+        # Insert processing steps
+        self._insertFunctionStep(self.prepareDataStep)
+        self._insertFunctionStep(self.membraneSegmentationStep)
+        self._insertFunctionStep(self.createOutputStep)
+
+    def prepareDataStep(self):
+        fn = self.inputSetOfTomograms.get().getFileName()
+        return fn
+
+    def membraneSegmentationStep(self, fn):
+
+        pathDir = 'outFile.mrc'
+
+        params = ' -dir %s ' % fn
+        params = ' -out %s ' % pathDir
+        Plugin.runTardis('tardis_mem', )
+
+    def createOutputStep(self):
+        # register how many times the message has been printed
+        # Now count will be an accumulated value
+        timesPrinted = Integer(self.times.get() + self.previousCount.get())
+        self._defineOutputs(count=timesPrinted)
+
+    # --------------------------- INFO functions -----------------------------------
+    def _summary(self):
+        """ Summarize what the protocol has done"""
+        summary = []
+
+        if self.isFinished():
+            summary.append("This protocol has printed *%s* %i times." % (self.message, self.times))
+        return summary
+
+    def _methods(self):
+        methods = []
+
+        if self.isFinished():
+            methods.append("%s has been printed in this run %i times." % (self.message, self.times))
+            if self.previousCount.hasPointer():
+                methods.append("Accumulated count from previous runs were %i."
+                               " In total, %s messages has been printed."
+                               % (self.previousCount, self.count))
+        return methods
