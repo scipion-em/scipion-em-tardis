8ca94e3907bf9c44741eb3bef682e4568e4701cb
Vilax <jlvilasprieto@gmail.com>
2024-11-22 11:53:23 +0100

    Update __init__.py

--- tardis/protocols/__init__.py
+++ tardis/protocols/__init__.py
@@ -5,4 +5,3 @@
 # **************************************************************************
 
 from .protocol_membrane_prediction3D import ProtMembrans3d
->>>>>>> cd8689214bdca84769b302cb5c487a0255fe26da
